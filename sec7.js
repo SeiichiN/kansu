e3053bcef5a439374f6bb85f42e44a6b28f1d644
SeiichiN <billie175@gmail.com>
2019-01-29 08:00:47 +0900

    sec7.js modified.

--- sec7.js
+++ sec7.js
@@ -4,6 +4,150 @@
 
 var expect = require('expect.js');
 
+var list = {
+	empty: () => {
+		return (pattern) => {
+			return pattern.empty();
+		};
+	},
+	cons:  (value, list) => {
+		return (pattern) => {
+			return pattern.cons(value, list);
+		};
+	},
+	match:  (data, pattern) => {
+		return data(pattern);
+	},
+	head:  (alist) => {
+		return list.match(alist, {
+			empty: (_) => {
+				return null;
+			},
+			cons: (head, tail) => {
+				return head;
+			}
+		});
+	},
+	tail: (alist) => {
+		return list.match(alist, {
+			empty: (_) => {
+				return null;
+			},
+			cons: (head, tail) => {
+				return tail;
+			}
+		});
+	},
+	isEmpty: (alist) => {
+		return list.match(alist, {
+			empty: (_) => {
+				return true;
+			},
+			cons: (head, tail) => {
+				return false;
+			}
+		});
+	},
+	append: (xs) => {
+		return (ys) => {
+			return list.match(xs, {
+				empty: (_) => {
+					return ys;
+				},
+				cons: (head, tail) => {
+					return cons(head, append(tail, ys));
+				}
+
+			});
+		};
+	},
+	map: (alist) => {
+		return (transform) => {
+			return match(alist, {
+				empty: (_) => { return empty(); },
+				cons: (head, tail) => {
+					return cons(transform(head),
+						map(tail, transform));
+				}
+
+			});
+		};
+	},
+	reverse: (alist) => {
+		var reverseHelper = (alist, accumulator) => {
+			return list.match(alist, {
+				empty: (_) => {
+					return accumulator;
+				},
+				cons: (head, tail) => {
+					return reverseHelper(tail, list.cons(head, accumulator));
+				}
+			});
+		};
+		return reverseHelper(alist, list.empty());
+	},
+	toArray: (alist) => {
+		var toArrayHelper = (alist, accumulator) => {
+			return list.match(alist, {
+				empty: (_) => {
+					return accumulator;
+				},
+				cons: (head, tail) => {
+					return toArrayHelper(tail,
+						accumulator.concat(head));
+				}
+			});
+		};
+		return toArrayHelper(alist, []);
+	}
+};
+var stream = {
+	match: (data, pattern) => {
+		return data(pattern);
+	},
+	empty: (_) => {
+		return (pattern) => {
+			return pattern.empty();
+		};
+	},
+	cons: (head, tailThunk) => {
+		return (pattern) => {
+			return pattern.cons(head, tailThunk);
+		};
+	},
+	/* head:: STREAM[T] => T */
+	head: (aStream) => {
+		return stream.match(aStream, {
+			empty: (_) => { return null; },
+			cons: (value, tailThunk) => { return value; }
+		});
+	},
+	/* tail:: STREAM[T] => STREAM[T] */
+	tail: (aStream) => {
+		return stream.match(aStream, {
+			empty: (_) => { return null; },
+			cons: (head, tailThunk) => {
+				return tailThunk();
+			}
+		});
+	},
+	take: (aStream, n) => {
+		return stream.match(aStream, {
+			empty: (_) => {
+				return list.empty();
+			},
+			cons: (head, tailThunk) => {
+				if (n === 0) {
+					return list.empty();
+				} else {
+					return  list.cons(head,
+						stream.take(tailThunk(), (n - 1)));
+				}
+			}
+		});
+	}
+};
+
 describe('カリー化で関数を返す', () => {
 	it('multipleOf関数の定義', (next) => {
 		var multipleOf = (n, m) => {
@@ -219,5 +363,106 @@ describe('関数を合成する', () => {
 		);
 		next();
 	});
-
+	it('反数関数の合成', (next) => {
+		// 反数の定義
+		var opposite = (n) => {
+			return - n;
+		};
+		expect (
+			compose(opposite, opposite)(2)
+		).to.eql(
+			2
+		);
+		next();
+	});
+	it('カリー化による合成', (next) => {
+		var opposite = (n) => {
+			return - n;
+		};
+		var addCurried = (x) => {
+			return (y) => {
+				return x + y;
+			};
+		};
+		expect(
+			compose(opposite, addCurried(2))(3)
+		).to.eql(
+			-5
+		);
+		next();
+	});
+	it('具体的なlast関数', (next) => {
+		var last = (alist) => {
+			return list.match(alist, {
+				empty: (_) => {
+					return null;
+				},
+				cons: (head, tail) => {
+					return list.match(tail, {
+						empty: (_) => {
+							return head;
+						},
+						cons: (_, __) => {
+							return last(tail);
+						}
+					});
+				}
+			});
+		};
+		var alist = 
+			list.cons(1,
+				list.cons(2,
+					list.cons(3,
+						list.empty())));
+		expect(
+			last(alist)
+		).to.eql(
+			3
+		);
+		next();
+	});
+	it('抽象的なlast関数', (next) => {
+		var last = (alist) => {
+			return compose(list.head, list.reverse)(alist);
+		};
+		var alist = 
+			list.cons(1,
+				list.cons(2,
+					list.cons(3,
+						list.empty())));
+		expect(
+			last(alist)
+		).to.eql(
+			3
+		);
+		next();
+	});
+	it('Yコンビネーター', (next) => {
+		var Y = (F) => {
+			return ((x) => {
+				return F((y) => {
+					return x(x)(y);
+				});
+			})((x) => {
+				return F((y) => {
+					return x(x)(y);
+				});
+			});
+		};
+		var factorial = Y((fact) => {
+			return (n) => {
+				if (n === 0) {
+					return 1;
+				} else {
+					return n * fact(n - 1);
+				}
+			};
+		});
+		expect(
+			factorial(3)   // 3 * 2 * 1 = 6
+		).to.eql(
+			6
+		);
+		next();
+	});
 });
